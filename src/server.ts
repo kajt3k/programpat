994219f39dc7ff3792764219cd715d25514f4c85
kajt3k <kajetanwuttkowski@gmail.com>
2025-04-28 19:33:00 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,11 @@ const start = async () => {
 
   // Add your own express routes here
 
-  app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`✅ Server is running on port ${port}`);
+});
+
 };
 
 start();
